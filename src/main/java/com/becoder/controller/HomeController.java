99e853466d219e2df0af14cd62f8bc553db857e5
Pabitra Das <codewithpabitra@gmail.com>
2024-09-25 00:47:17 +0530

    inital commit

---
+++ src/main/java/com/becoder/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.becoder.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public ResponseEntity<?> home()
+	{
+		return new ResponseEntity<>("Welcome to becoder",HttpStatus.OK);
+	}
+	
+}
